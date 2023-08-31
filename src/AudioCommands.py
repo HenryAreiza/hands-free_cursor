90b6674abcb68adc5703021ba7f777b0f5a8054a
HenryAreiza <henryareiza2493@gmail.com>
2023-08-31 18:39:15 +0200

    [ADD][UPD] First audio model configuration

---
+++ src/AudioCommands.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import sounddevice as sd
+from scipy.signal import decimate
+from transformers import pipeline
+
+MODELS_PATH = os.path.join('..', 'models')
+
+# Load the pipeline
+pipe = pipeline("audio-classification", model=os.path.join(MODELS_PATH, 'audio_model'))
+
+def classify_audio(audio_data):
+    result = pipe(audio_data)[0]["label"]
+    return result
+
+# Define the callback function
+audio_buffer = [None, None]
+def audio_callback(indata, _, __, ___):
+    audio_buffer[0] = audio_buffer[1]
+    audio_buffer[1] = indata.copy()
+
+# Define audio InputStream
+audio_stream = sd.InputStream(callback=audio_callback, channels=1,
+                              samplerate=48000, blocksize=24000)
+
+audio_stream.start()
+sd.sleep(int(2000))
+counter = 0
+print('\nListening...')
+while counter<10:
+    sd.sleep(int(500))
+    audio_data = np.concatenate(audio_buffer, axis=0)
+    if audio_data.max() > 0.2:
+        sd.sleep(int(500))
+        audio_data = np.concatenate(audio_buffer, axis=0)
+        audio_data = decimate(audio_data[:,0], 3)
+        result = classify_audio(audio_data)
+        print(f"{counter} - Command: {result}")
+        sd.sleep(int(500))
+        counter += 1
+
+audio_stream.stop()
+audio_stream.close()
+print('\n')
+
