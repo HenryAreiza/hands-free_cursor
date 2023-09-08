afbedcb7076407d1ef6ef578b5d9f150395901ae
Deivid <deivid.johan.botina.monsalve@gmail.com>
2023-09-08 17:46:01 +0200

    [UPD] spaces_test/simple_microphone.py showing only the last word prediction in real time

--- spaces_test/simple_microphone.py
+++ spaces_test/simple_microphone.py
@@ -4,24 +4,15 @@ import time
 
 p = pipeline("automatic-speech-recognition")
 
-def transcribe(audio, state=""):
+def transcribe(audio):
     time.sleep(1)
     text = p(audio)["text"]
-    state += text + " "
-    return state, state
+    return text
 
 demo = gr.Interface(
     fn=transcribe,
-    inputs=[
-        gr.Audio(source="microphone",
-                  type="filepath",
-                    streaming=True),
-        "state"
-    ],
-    outputs=[
-        "textbox",
-        "state"
-    ],
+    inputs = gr.Audio(source="microphone",type="filepath",streaming=True),
+    outputs= "textbox",
     live=True)
 
 demo.launch()
